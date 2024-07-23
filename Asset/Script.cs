7434fab55ef49901ec2296a99b0856069608c2ca
ribbondash@gmail.com <ribbondash@gmail.com>
2024-07-23 21:08:21 +0900

    スクリプト読み込み

---
+++ Asset/Script.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace SeaDrop
+{
+    public class DrawScript
+    {
+
+    }
+
+    public class Script
+    {
+        public static void Run(string path)
+        {
+            var ssr = ScriptSourceResolver.Default
+                .WithBaseDirectory(Environment.CurrentDirectory);
+            var smr = ScriptMetadataResolver.Default
+                .WithBaseDirectory(Environment.CurrentDirectory);
+            var so = ScriptOptions.Default
+                .WithSourceResolver(ssr)
+                .WithMetadataResolver(smr);
+            /*var so = ScriptOptions.Default
+                .WithFilePath(Path.GetFullPath(path));*/
+
+            var script = CSharpScript.Create(File.ReadAllText(path), so);
+            script.RunAsync().Wait();
+        }
+    }
+}
